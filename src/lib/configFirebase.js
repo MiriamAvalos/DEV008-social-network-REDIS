a97ea0bd62dd187f108eb95efae8e235b5b1a161
Mirisaurio <Mirisaurio>
2023-08-04 19:02:24 -0600

    Inicio de autenticación con correo y contraseña en Firebase

--- src/lib/configFirebase.js
+++ src/lib/configFirebase.js
@@ -19,3 +19,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
+// Autenticación con correo y contraseña
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+//console.log(auth);
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    
+  });
